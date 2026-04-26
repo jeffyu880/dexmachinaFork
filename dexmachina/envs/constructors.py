8663e4d56a1e8b5bac5980b73622f8aee0c610cc
Jeffrey Siyuan Yu <jsyu@izar1.izar.cluster>
2026-04-26 17:30:01 +0200

    multi-demo

--- dexmachina/envs/constructors.py
+++ dexmachina/envs/constructors.py
@@ -309,6 +309,7 @@ def get_common_argparser():
     parser.add_argument('--rand_init_ratio', '-randr', type=float, default=0.0)   
     parser.add_argument('--chunk_ep_length', '-chunk', type=int, default=-1)
     parser.add_argument('--demo_sampling', type=str, default='random', choices=['random', 'deterministic'])
+    parser.add_argument('--multi_demo', '-md', action='store_true', help='Use multiple demos for training')
     
     parser.add_argument('--use_rand', '-rand', action='store_true')
     parser.add_argument('--randomize_observations', '-rand_obs', action='store_true')
